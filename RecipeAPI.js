0f0da6349b03b07260827912cf2dac8eb3c93f41
jpd5749 <jpd5749@psu.edu>
2022-02-15 19:27:49 -0500

    Added a searching functionality

--- RecipeAPI.js
+++ RecipeAPI.js
@@ -3,13 +3,15 @@ let data;
 //variables that correspond to the elements in the HTML
 let categoryList = document.getElementById("categoryList");
 let description = document.getElementById("categoryDescription");
+let searchBox = document.getElementById("searchBox");
+let foundMealDescription = document.getElementById("foundMealDescription");
+//http request
+const request = new XMLHttpRequest();
 
 
 //only called once when the page is loaded
 function populateSelect()
 {
-    const request = new XMLHttpRequest();
-
     request.open("GET","https://www.themealdb.com/api/json/v1/1/categories.php",true);
 
     request.onload = function() {
@@ -21,7 +23,6 @@ function populateSelect()
             data.categories.forEach(
                 category=>
                 {
-                    console.log(`Category name: ${category.strCategory}, Desc: ${category.strCategoryDescription}`);
                     let option = document.createElement("option");
                     //set the value to the description for ease of use of the "selectCategory" function
                     option.value = category.strCategoryDescription;
@@ -45,6 +46,42 @@ function selectCategory()
     let firstChild = description.childNodes[0];
     //now, replace the first child with the new text node
     description.replaceChild(descriptionText, firstChild);
+}
+
+function searchMeal()
+{
+    request.open("GET","https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchBox.value,true);
+
+    request.onload = function() {
+        data = JSON.parse(this.response);
+        
+        if(request.status == 200 && data.meals == null)
+        {
+            //variable that is the description of the selected
+            let descriptionText = document.createTextNode("No Results");
+            //variable that is the first child of the paragraph
+            let firstChild = foundMealDescription.childNodes[0];
+            //now, replace the first child with the new text node
+            foundMealDescription.replaceChild(descriptionText, firstChild);
+        }
+
+        else if(request.status == 200 && data.meals != null)
+        {
+            let foundMeal = data.meals[0];
+            console.log("Meal Name: " + foundMeal.strMeal + " Instructions: " + foundMeal.strInstructions);
+            //variable that is the description of the selected
+            let descriptionText = document.createTextNode("Meal Name: " + foundMeal.strMeal + " Instructions: " + foundMeal.strInstructions);
+            //variable that is the first child of the paragraph
+            let firstChild = foundMealDescription.childNodes[0];
+            //now, replace the first child with the new text node
+            foundMealDescription.replaceChild(descriptionText, firstChild);
+        }
+        else{
+            console.log(`Error occurced: Status: ${request.status}`);
+        }
+    };
+
+    request.send();
 }
 
 //populate the select when the page loads
