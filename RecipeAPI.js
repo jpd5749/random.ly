a418e98eb0c7641fba032fa1f26b168c64ef6a3b
jpd5749 <jpd5749@psu.edu>
2022-02-24 00:00:35 -0500

    Added a random result function. Changed the formatting of the HTML file to seperate the name of the receipe and it's instructions.

--- RecipeAPI.js
+++ RecipeAPI.js
@@ -4,6 +4,7 @@ let data;
 let categoryList = document.getElementById("categoryList");
 let description = document.getElementById("categoryDescription");
 let searchBox = document.getElementById("searchBox");
+let foundMealName = document.getElementById("foundMealName");
 let foundMealDescription = document.getElementById("foundMealDescription");
 //http request
 const request = new XMLHttpRequest();
@@ -58,11 +59,13 @@ function searchMeal()
         if(request.status == 200 && data.meals == null)
         {
             //variable that is the description of the selected
-            let descriptionText = document.createTextNode("No Results");
+            let nameText = document.createTextNode("No Results");
+            let descriptionText = document.createTextNode(" ");
             //variable that is the first child of the paragraph
-            let firstChild = foundMealDescription.childNodes[0];
-            //now, replace the first child with the new text node
-            foundMealDescription.replaceChild(descriptionText, firstChild);
+            let firstChild = foundMealName.childNodes[0];
+            foundMealName.replaceChild(nameText, firstChild);
+            let secondChild = foundMealDescription.childNodes[0];
+            foundMealDescription.replaceChild(descriptionText, secondChild);
         }
 
         else if(request.status == 200 && data.meals != null)
@@ -70,11 +73,56 @@ function searchMeal()
             let foundMeal = data.meals[0];
             console.log("Meal Name: " + foundMeal.strMeal + " Instructions: " + foundMeal.strInstructions);
             //variable that is the description of the selected
-            let descriptionText = document.createTextNode("Meal Name: " + foundMeal.strMeal + " Instructions: " + foundMeal.strInstructions);
+            let nameText = document.createTextNode("Meal Name: " + foundMeal.strMeal);
+            let descriptionText = document.createTextNode("Instructions: " + foundMeal.strInstructions);
             //variable that is the first child of the paragraph
-            let firstChild = foundMealDescription.childNodes[0];
-            //now, replace the first child with the new text node
-            foundMealDescription.replaceChild(descriptionText, firstChild);
+            let firstChild = foundMealName.childNodes[0];
+            //replace the first child of the paragraph element
+            foundMealName.replaceChild(nameText, firstChild);
+            let secondChild = foundMealDescription.childNodes[0];
+            foundMealDescription.replaceChild(descriptionText, secondChild);
+        }
+        else{
+            console.log(`Error occurced: Status: ${request.status}`);
+        }
+    };
+
+    request.send();
+}
+
+function randomMeal()
+{
+    request.open("GET","https://www.themealdb.com/api/json/v1/1/random.php",true);
+
+    request.onload = function() {
+        data = JSON.parse(this.response);
+        
+        if(request.status == 200 && data.meals == null)
+        {
+            //variable that is the description of the selected
+            let nameText = document.createTextNode("No Results");
+            let descriptionText = document.createTextNode(" ");
+            //variable that is the first child of the paragraph
+            let firstChild = foundMealName.childNodes[0];
+            foundMealName.replaceChild(nameText, firstChild);
+            let secondChild = foundMealDescription.childNodes[0];
+            foundMealDescription.replaceChild(descriptionText, secondChild);
+        }
+
+        else if(request.status == 200 && data.meals != null)
+        {
+            let foundMeal = data.meals[0];
+            console.log("Meal Name: " + foundMeal.strMeal + " Instructions: " + foundMeal.strInstructions);
+            //variable that is the description of the selected
+            let nameText = document.createTextNode("Meal Name: " + foundMeal.strMeal);
+            let descriptionText = document.createTextNode("Instructions: " + foundMeal.strInstructions);
+            //variable that is the first child of the paragraph element
+            let firstChild = foundMealName.childNodes[0];
+            //replace the first child with our new text node
+            foundMealName.replaceChild(nameText, firstChild);
+            let secondChild = foundMealDescription.childNodes[0];
+            foundMealDescription.replaceChild(descriptionText, secondChild);
+
         }
         else{
             console.log(`Error occurced: Status: ${request.status}`);
