c9692fe2c4a5887b1c098b6561e5544e03d10e01
jpd5749 <jpd5749@psu.edu>
2022-03-01 18:12:49 -0500

    Added basic Spotify API Integration

--- RecipeAPI.js
+++ RecipeAPI.js
@@ -6,13 +6,17 @@ let description = document.getElementById("categoryDescription");
 let searchBox = document.getElementById("searchBox");
 let foundMealName = document.getElementById("foundMealName");
 let foundMealDescription = document.getElementById("foundMealDescription");
-//http request
-const request = new XMLHttpRequest();
+let spotifyList = document.getElementById("spotifyList");
+//spotify credentials
+var client_id = 'd8cd43d646624fe085630310d15cc9e9';
+var client_secret = '193c9a9963454cf488aaad0efdc54442';
+var spotify_token;
 
 
 //only called once when the page is loaded
 function populateSelect()
 {
+    const request = new XMLHttpRequest();
     request.open("GET","https://www.themealdb.com/api/json/v1/1/categories.php",true);
 
     request.onload = function() {
@@ -51,6 +55,7 @@ function selectCategory()
 
 function searchMeal()
 {
+    const request = new XMLHttpRequest();
     request.open("GET","https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchBox.value,true);
 
     request.onload = function() {
@@ -71,7 +76,6 @@ function searchMeal()
         else if(request.status == 200 && data.meals != null)
         {
             let foundMeal = data.meals[0];
-            console.log("Meal Name: " + foundMeal.strMeal + " Instructions: " + foundMeal.strInstructions);
             //variable that is the description of the selected
             let nameText = document.createTextNode("Meal Name: " + foundMeal.strMeal);
             let descriptionText = document.createTextNode("Instructions: " + foundMeal.strInstructions);
@@ -92,6 +96,7 @@ function searchMeal()
 
 function randomMeal()
 {
+    const request = new XMLHttpRequest();
     request.open("GET","https://www.themealdb.com/api/json/v1/1/random.php",true);
 
     request.onload = function() {
@@ -132,5 +137,57 @@ function randomMeal()
     request.send();
 }
 
+function spotifyToken()
+{
+    const request = new XMLHttpRequest();
+    request.open("POST","https://accounts.spotify.com/api/token?grant_type=client_credentials&client_id=" + client_id + "&client_secret=" + client_secret,true);
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    request.onload = function() {
+        data = JSON.parse(this.response);
+
+        if(request.status == 200)
+        {
+            spotify_token = data.access_token;
+        }
+        else{
+            console.log(`Error occurced: Status: ${request.status}`);
+            console.log(data);
+        }
+    };
+    request.send();
+}
+
+function spotifyGetGenres()
+{
+    const request = new XMLHttpRequest();
+    request.open("GET","https://api.spotify.com/v1/recommendations/available-genre-seeds",true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Authorization", "Bearer " + spotify_token +"");
+
+    request.onload = function() {
+        data = JSON.parse(this.response);
+
+        if(request.status == 200)
+        {
+            data.genres.forEach(
+                genre=>
+                {
+                    console.log(genre);
+                    let option = document.createElement("option");
+                    //set the value to the description for ease of use of the "selectCategory" function
+                    option.innerHTML = genre;
+                    spotifyList.appendChild(option);
+                }
+            );
+        }
+        else{
+            console.log(`Error occurced: Status: ${request.status}`);
+        }
+    };
+    request.send();
+}
+
 //populate the select when the page loads
 populateSelect();
+spotifyToken();
