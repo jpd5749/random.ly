258a0efce1f8666bff1ebe728ddf511904f8feca
jpd5749 <jpd5749@psu.edu>
2022-04-06 01:17:51 -0400

    Added a bunch of new features

--- RecipeAPI.js
+++ RecipeAPI.js
@@ -7,6 +7,12 @@ let searchBox = document.getElementById("searchBox");
 let foundMealName = document.getElementById("foundMealName");
 let foundMealDescription = document.getElementById("foundMealDescription");
 let spotifyList = document.getElementById("spotifyList");
+let movieList = document.getElementById("movieList");
+let movieName = document.getElementById("movieName");
+let movieDescription = document.getElementById("movieDescription");
+let movieSearchBox = document.getElementById("movieSearchBox");
+let movieImage = document.getElementById("movieImage");
+let mealImage = document.getElementById("mealImage");
 //spotify credentials
 var client_id = 'd8cd43d646624fe085630310d15cc9e9';
 var client_secret = '193c9a9963454cf488aaad0efdc54442';
@@ -31,7 +37,7 @@ function populateSelect()
                 {
                     let option = document.createElement("option");
                     //set the value to the description for ease of use of the "selectCategory" function
-                    option.value = category.strCategoryDescription;
+                    option.value = category.strCategory;
                     option.innerHTML = category.strCategory;
                     categoryList.appendChild(option);
                 }
@@ -77,6 +83,7 @@ function searchMeal()
         else if(request.status == 200 && data.meals != null)
         {
             let foundMeal = data.meals[0];
+
             //variable that is the description of the selected
             let nameText = document.createTextNode("Meal Name: " + foundMeal.strMeal);
             let descriptionText = document.createTextNode("Instructions: " + foundMeal.strInstructions);
@@ -86,6 +93,7 @@ function searchMeal()
             foundMealName.replaceChild(nameText, firstChild);
             let secondChild = foundMealDescription.childNodes[0];
             foundMealDescription.replaceChild(descriptionText, secondChild);
+            mealImage.src = foundMeal.strMealThumb;
         }
         else{
             console.log(`Error occurced: Status: ${request.status}`);
@@ -98,7 +106,7 @@ function searchMeal()
 function randomMeal()
 {
     const request = new XMLHttpRequest();
-    request.open("GET","https://www.themealdb.com/api/json/v1/1/random.php",true);
+    request.open("GET","https://www.themealdb.com/api/json/v1/1/filter.php?c=" + categoryList.value,true);
 
     request.onload = function() {
         data = JSON.parse(this.response);
@@ -117,18 +125,52 @@ function randomMeal()
 
         else if(request.status == 200 && data.meals != null)
         {
-            let foundMeal = data.meals[0];
-            console.log("Meal Name: " + foundMeal.strMeal + " Instructions: " + foundMeal.strInstructions);
-            //variable that is the description of the selected
-            let nameText = document.createTextNode("Meal Name: " + foundMeal.strMeal);
-            let descriptionText = document.createTextNode("Instructions: " + foundMeal.strInstructions);
-            //variable that is the first child of the paragraph element
-            let firstChild = foundMealName.childNodes[0];
-            //replace the first child with our new text node
-            foundMealName.replaceChild(nameText, firstChild);
-            let secondChild = foundMealDescription.childNodes[0];
-            foundMealDescription.replaceChild(descriptionText, secondChild);
+            //generate random number based on length of meals in category
+            let random = Math.floor(Math.random() * data.meals.length);
+            //let found meal be one of the randombly chosen meals from the category
+            let foundMeal = data.meals[random];
+            let randomMeal;
 
+            //because foundMeal only has the meal ID, we have to call the API again to get the rest of the information.
+            const request2 = new XMLHttpRequest();
+            request2.open("GET","https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + foundMeal.idMeal,true);
+        
+            request2.onload = function() {
+                data2 = JSON.parse(this.response);
+                
+                if(request2.status == 200 && data2.meals == null)
+                {
+                    //variable that is the description of the selected
+                    let nameText = document.createTextNode("No Results");
+                    let descriptionText = document.createTextNode(" ");
+                    //variable that is the first child of the paragraph
+                    let firstChild = foundMealName.childNodes[0];
+                    foundMealName.replaceChild(nameText, firstChild);
+                    let secondChild = foundMealDescription.childNodes[0];
+                    foundMealDescription.replaceChild(descriptionText, secondChild);
+                }
+        
+                else if(request2.status == 200 && data2.meals != null)
+                {
+                    //if it returns a request, it has found a random meal based on the category
+                    let randomMeal = data2.meals[0];
+                    console.log("Meal Name: " + randomMeal.strMeal + " Instructions: " + randomMeal.strInstructions);
+                    //variable that is the description of the selected
+                    let nameText = document.createTextNode("Meal Name: " + randomMeal.strMeal);
+                    let descriptionText = document.createTextNode("Instructions: " + randomMeal.strInstructions);
+                    //variable that is the first child of the paragraph element
+                    let firstChild = foundMealName.childNodes[0];
+                    //replace the first child with our new text node
+                    foundMealName.replaceChild(nameText, firstChild);
+                    let secondChild = foundMealDescription.childNodes[0];
+                    foundMealDescription.replaceChild(descriptionText, secondChild);
+                    mealImage.src = randomMeal.strMealThumb;
+                }
+                else{
+                    console.log(`Error occurced: Status: ${request2.status}`);
+                }
+            };
+            request2.send();
         }
         else{
             console.log(`Error occurced: Status: ${request.status}`);
@@ -209,6 +251,151 @@ function spotifyWait()
     }
 }
 
+function getMovieGenres()
+{
+    let data = null;
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("GET", "https://advanced-movie-search.p.rapidapi.com/genre/movie/list");
+    xhr.setRequestHeader("X-RapidAPI-Host", "advanced-movie-search.p.rapidapi.com");
+    xhr.setRequestHeader("X-RapidAPI-Key", "62ade45960mshc0aaf491884fb2ep14bc5cjsnef200e9e5f59");
+
+    xhr.onload = function() {
+        data = JSON.parse(this.response);
+
+        if(xhr.status == 200)
+        {
+            data.genres.forEach(
+                genre=>
+                {
+                    console.log(genre.name);
+                    let option = document.createElement("option");
+                    //set the value to the description for ease of use of the "selectCategory" function
+                    option.innerHTML = genre.name;
+                    option.value = genre.id;
+                    movieList.appendChild(option);
+                }
+            );
+        }
+        else{
+            console.log(`Error occurced: Status: ${xhr.status}`);
+        }
+    };
+
+    xhr.send(data);
+}
+
+function selectMovieGenre()
+{
+    let data = null;
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("GET", "https://advanced-movie-search.p.rapidapi.com/discover/movie?with_genres=" + movieList.value);
+    xhr.setRequestHeader("X-RapidAPI-Host", "advanced-movie-search.p.rapidapi.com");
+    xhr.setRequestHeader("X-RapidAPI-Key", "62ade45960mshc0aaf491884fb2ep14bc5cjsnef200e9e5f59");
+
+    xhr.onload = function() {
+        data = JSON.parse(this.response);
+
+        if(xhr.status == 200)
+        {
+            //first, generate random number based on how many elements are in the array
+            let random = Math.floor(Math.random() * data.results.length);
+            //variable that is the description of the selected
+            let nameText = document.createTextNode(data.results[random].original_title);
+            let descriptionText = document.createTextNode(data.results[random].overview);
+            //variable that is the first child of the paragraph
+            let firstChild = movieName.childNodes[0];
+            let secondFirstChild = movieDescription.childNodes[0];
+            //now, replace the first child with the new text node
+            movieName.replaceChild(nameText, firstChild);
+            movieDescription.replaceChild(descriptionText, secondFirstChild);
+            movieImage.src = data.results[random].poster_path;
+        }
+        else{
+            //variable that is the description of the selected
+            let nameText = document.createTextNode("No Results");
+            let descriptionText = document.createTextNode(" ");
+            //variable that is the first child of the paragraph
+            let firstChild = movieName.childNodes[0];
+            movieName.replaceChild(nameText, firstChild);
+            let secondChild = movieDescription.childNodes[0];
+            movieDescription.replaceChild(descriptionText, secondChild);
+            movieImage.src = "";
+            console.log(`Error occurced: Status: ${xhr.status}`);
+        }
+    };
+
+    xhr.send(data);
+}
+
+function searchMovie()
+{
+    let data = null;
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("GET", "https://advanced-movie-search.p.rapidapi.com/search/movie?query=" + movieSearchBox.value);
+    xhr.setRequestHeader("X-RapidAPI-Host", "advanced-movie-search.p.rapidapi.com");
+    xhr.setRequestHeader("X-RapidAPI-Key", "62ade45960mshc0aaf491884fb2ep14bc5cjsnef200e9e5f59");
+
+    xhr.onload = function() {
+        data = JSON.parse(this.response);
+
+        if((xhr.status == 200) && (data.total_results != 0))
+        {
+            //variable that is the description of the selected
+            let nameText = document.createTextNode(data.results[0].original_title);
+            let descriptionText = document.createTextNode(data.results[0].overview);
+            //variable that is the first child of the paragraph
+            let firstChild = movieName.childNodes[0];
+            let secondFirstChild = movieDescription.childNodes[0];
+            //now, replace the first child with the new text node
+            movieName.replaceChild(nameText, firstChild);
+            movieDescription.replaceChild(descriptionText, secondFirstChild);
+            movieImage.src = data.results[0].poster_path;
+        }
+        else{
+            //variable that is the description of the selected
+            let nameText = document.createTextNode("No Results");
+            let descriptionText = document.createTextNode(" ");
+            //variable that is the first child of the paragraph
+            let firstChild = movieName.childNodes[0];
+            movieName.replaceChild(nameText, firstChild);
+            let secondChild = movieDescription.childNodes[0];
+            movieDescription.replaceChild(descriptionText, secondChild);
+            movieImage.src = "";
+            console.log(`Error occurced: Status: ${xhr.status}`);
+        }
+    };
+
+    xhr.send(data);
+}
+
 //populate the select when the page loads
 populateSelect();
 spotifyToken();
+getMovieGenres();
